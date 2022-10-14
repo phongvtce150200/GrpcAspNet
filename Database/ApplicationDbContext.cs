efe5729a2cc28b9dcb26ec0425572808c8258581
Phong Thanh Vo <phongvtce150200@fpt.edu.vn>
2022-10-14 18:21:18 +0700

    First Commit

---
+++ Database/ApplicationDbContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Demo2Grpc.Models;
+
+#nullable disable
+
+namespace Demo2Grpc.Database
+{
+    public partial class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=THANHPHONGVO\\SQLEXPRESS;Database=Demo2GRPC;Uid=sa;Pwd=sa;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.ToTable("Customer");
+
+                entity.Property(e => e.Id)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Address)
+                    .IsRequired()
+                    .HasMaxLength(250);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(150);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Services/CustomerService.cs
@@ -0,0 +1,85 @@
+﻿using Demo2Grpc.Database;
+using Demo2Grpc.Protos;
+using Grpc.Core;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using static Demo2Grpc.Protos.GrpcCustomer;
+
+namespace Demo2Grpc.Services
+{
+    public class CustomerService : GrpcCustomerBase
+    {
+        public readonly ApplicationDbContext _context;
+
+        public CustomerService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public override Task<CustomerList> GetAll(Demo2Grpc.Protos.Empty requestData, ServerCallContext context)
+        {
+            CustomerList response = new CustomerList();
+            var customerlist = _context.Customers.Select(c => new Customer()
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Address = c.Address
+            });
+
+            response.Customers.AddRange(customerlist);
+            return Task.FromResult(response);
+        }
+
+        public override Task<Demo2Grpc.Protos.Customer> GetCustomer(IDRequest request, ServerCallContext context)
+        {
+            var obj = _context.Customers.FirstOrDefault(c => c.Id == request.Id);
+            Demo2Grpc.Protos.Customer cus = new Demo2Grpc.Protos.Customer()
+            {
+                Id = obj.Id,
+                Name = obj.Name,
+                Address = obj.Address
+            };
+
+            return Task.FromResult(cus);
+        }
+
+        [HttpPost]
+        public override Task<Empty> AddCustomer(Customer request, ServerCallContext context)
+        {
+            Demo2Grpc.Models.Customer cus = new Demo2Grpc.Models.Customer()
+            {
+                Id = request.Id,
+                Name = request.Name,
+                Address = request.Address
+
+            };
+            _context.Customers.Add(cus);
+            _context.SaveChanges();
+            return Task.FromResult(new Empty());
+        }
+
+        public override Task<Empty> UpdateCustomer(Customer request, ServerCallContext context)
+        {
+
+            Demo2Grpc.Models.Customer cus = new Demo2Grpc.Models.Customer()
+            {
+                Id = request.Id,
+                Name = request.Name,
+                Address = request.Address
+
+            };
+            _context.Customers.Update(cus);
+            _context.SaveChanges();
+            return Task.FromResult(new Empty());
+        }
+
+        public override Task<Empty> DeleteCustomer(IDRequest request, ServerCallContext context)
+        {
+            var cus = _context.Customers.Find(request.Id);
+            _context.Customers.Remove(cus);
+            _context.SaveChanges();
+            return Task.FromResult(new Empty());
+        }
+    }
+}
